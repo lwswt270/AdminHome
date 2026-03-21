8129f3a598431854dc49e289c51bb4c3d6143e50
lwswt270 <lwswt270@qq.com>
2026-03-21 21:26:27 +0800

    feat: Add App.vue

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="app">
+    <h1>AdminHome</h1>
+    <p>Vue3 管理后台项目</p>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style>
+.app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
